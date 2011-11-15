c0f10fc98fbdcacbf4b6819327de55712a2fee37
Darin Cassler <darin@cassler.net>
2011-11-15 13:53:29 -0500

    bare loop added

--- index.php
+++ index.php
@@ -17,7 +17,17 @@
 </head>
 <body>
 
-
+<?php query_posts('showposts=20'); ?>
+<?php if ( have_posts() ) : ?>
+<?php while ( have_posts() ) : the_post(); ?>
+	<article>
+		<h2><?php the_title(); ?></h2>
+		<?php the_content(); ?>
+		
+	</article>
+<?php endwhile; ?>
+<?php else : ?>			
+<?php endif; ?>
 
 <div id="scripts">
 	<!-- JavaScript at the bottom for fast page loading -->
